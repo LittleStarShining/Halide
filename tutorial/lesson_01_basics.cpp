249d36c626335531d72640aa541b8820d2328134
LittleStarShining <138993813+LittleStarShining@users.noreply.github.com>
2024-02-25 12:52:14 +0800

    Update lesson_01_basics.cpp
    transfer

--- tutorial/lesson_01_basics.cpp
+++ tutorial/lesson_01_basics.cpp
@@ -1,95 +1,26 @@
-// Halide tutorial lesson 1: Getting started with Funcs, Vars, and Exprs
-
-// This lesson demonstrates basic usage of Halide as a JIT compiler for imaging.
-
-// On linux, you can compile and run it like so:
-// g++ lesson_01*.cpp -g -I <path/to/Halide.h> -L <path/to/libHalide.so> -lHalide -lpthread -ldl -o lesson_01 -std=c++17
-// LD_LIBRARY_PATH=<path/to/libHalide.so> ./lesson_01
-
-// On os x:
-// g++ lesson_01*.cpp -g -I <path/to/Halide.h> -L <path/to/libHalide.so> -lHalide -o lesson_01 -std=c++17
-// DYLD_LIBRARY_PATH=<path/to/libHalide.dylib> ./lesson_01
-
-// If you have the entire Halide source tree, you can also build it by
-// running:
-//    make tutorial_lesson_01_basics
-// in a shell with the current directory at the top of the halide
-// source tree.
-
-// The only Halide header file you need is Halide.h. It includes all of Halide.
 #include "Halide.h"
-
-// We'll also include stdio for printf.
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-    // This program defines a single-stage imaging pipeline that
-    // outputs a grayscale diagonal gradient.
-
-    // A 'Func' object represents a pipeline stage. It's a pure
-    // function that defines what value each pixel should have. You
-    // can think of it as a computed image.
+    // 'Func'对象代表一个图像处理阶段。它是一个纯函数，定义了每个像素应该具有的数值
     Halide::Func gradient;
 
-    // Var objects are names to use as variables in the definition of
-    // a Func. They have no meaning by themselves.
+    // Var对象是在定义Func时用作变量的名称。
     Halide::Var x, y;
-
-    // We typically use Vars named 'x' and 'y' to correspond to the x
-    // and y axes of an image, and we write them in that order. If
-    // you're used to thinking of images as having rows and columns,
-    // then x is the column index, and y is the row index.
-
-    // Funcs are defined at any integer coordinate of its variables as
-    // an Expr in terms of those variables and other functions.
-    // Here, we'll define an Expr which has the value x + y. Vars have
-    // appropriate operator overloading so that expressions like
-    // 'x + y' become 'Expr' objects.
+    
+    // 定义一个值为x + y的Expr
     Halide::Expr e = x + y;
 
-    // Now we'll add a definition for the Func object. At pixel x, y,
-    // the image will have the value of the Expr e. On the left hand
-    // side we have the Func we're defining and some Vars. On the right
-    // hand side we have some Expr object that uses those same Vars.
+    // 为Func对象添加一个定义。在像素坐标x, y处，图像的值将是Expr e的值
     gradient(x, y) = e;
-
-    // This is the same as writing:
-    //
-    //   gradient(x, y) = x + y;
-    //
-    // which is the more common form, but we are showing the
-    // intermediate Expr here for completeness.
-
-    // That line of code defined the Func, but it didn't actually
-    // compute the output image yet. At this stage it's just Funcs,
-    // Exprs, and Vars in memory, representing the structure of our
-    // imaging pipeline. We're meta-programming. This C++ program is
-    // constructing a Halide program in memory. Actually computing
-    // pixel data comes next.
-
-    // Now we 'realize' the Func, which JIT compiles some code that
-    // implements the pipeline we've defined, and then runs it.  We
-    // also need to tell Halide the domain over which to evaluate the
-    // Func, which determines the range of x and y above, and the
-    // resolution of the output image. Halide.h also provides a basic
-    // templatized image type we can use. We'll make an 800 x 600
-    // image.
+    
+    // 将创建一个 800 x 600 的图像。
     Halide::Buffer<int32_t> output = gradient.realize({800, 600});
-
-    // Halide does type inference for you. Var objects represent
-    // 32-bit integers, so the Expr object 'x + y' also represents a
-    // 32-bit integer, and so 'gradient' defines a 32-bit image, and
-    // so we got a 32-bit signed integer image out when we call
-    // 'realize'. Halide types and type-casting rules are equivalent
-    // to C.
-
-    // Let's check everything worked, and we got the output we were
-    // expecting:
+    
+    // check
     for (int j = 0; j < output.height(); j++) {
         for (int i = 0; i < output.width(); i++) {
-            // We can access a pixel of an Buffer object using similar
-            // syntax to defining and using functions.
             if (output(i, j) != i + j) {
                 printf("Something went wrong!\n"
                        "Pixel %d, %d was supposed to be %d, but instead it's %d\n",
@@ -98,9 +29,8 @@ int main(int argc, char **argv) {
             }
         }
     }
-
-    // Everything worked! We defined a Func, then called 'realize' on
-    // it to generate and run machine code that produced an Buffer.
+    
+    // 成功！定义了一个Func，然后在其上调用了'realize'，以生成并运行产生一个Buffer的机器码
     printf("Success!\n");
 
     return 0;
